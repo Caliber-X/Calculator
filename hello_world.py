098c3f9947bfebc9a90a2ff5a96cb8fdf967c8e3
Shuvam-WebO <shuvamd.wot2021@gmail.com>
2021-05-13 11:14:19 +0530

    added class functionality for better coding structure

--- hello_world.py
+++ hello_world.py
@@ -6,16 +6,31 @@ from PyQt5.QtWidgets import QApplication, QLabel, QWidget
 # 2. Create an instance of QApplication
 app = QApplication(sys.argv)
 
-# 3. Create an instance of your application's GUI
-window = QWidget()
-window.setWindowTitle("PyQt5 App")
-window.setGeometry(100, 100, 280, 80)
-window.move(60, 15)
-helloMsg = QLabel("<h1>Hello World</h1>", parent=window)
-helloMsg.move(60, 15)
+class main():
+    def __init__(self):
+        # 3. Create an instance of your application's GUI
+        self.window = QWidget()
+        self.window.setWindowTitle("PyQt5 App")
+        self.window.setGeometry(100, 100, 280, 80)
+        self.window.move(60, 15)
+        self.helloMsg = QLabel("<h1>Hello World</h1>", parent=self.window)
+        self.helloMsg.move(60, 15)
 
-# 4. Show your application's GUI
-window.show()
+        # 4. Show your application's GUI
+        self.window.show()
+
+obj = main()
+
+# # 3. Create an instance of your application's GUI
+# window = QWidget()
+# window.setWindowTitle("PyQt5 App")
+# window.setGeometry(100, 100, 280, 80)
+# window.move(60, 15)
+# helloMsg = QLabel("<h1>Hello World</h1>", parent=window)
+# helloMsg.move(60, 15)
+
+# # 4. Show your application's GUI
+# window.show()
 
 # 5. Run your application's event loop (or main loop)
 sys.exit(app.exec_())
