5da414db4a8b00da5824703eee117defdb5b4528
Shuvam-WebO <shuvamd.wot2021@gmail.com>
2021-05-14 11:32:02 +0530

    load UI from .ui using uic.loadUi

--- calc.py
+++ calc.py
@@ -2,26 +2,26 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import uic
 import sys
 
-# .ui -> .py
-import os
-from subprocess import getstatusoutput
-ui2py_flag = False
-if os.path.exists("calc.ui") and getstatusoutput("pyuic5 calc.ui -o ui_calc.py")[0] == False:
-    ui2py_flag = True
-if ui2py_flag == False and os.path.exists("ui_calc.py") == False:
-    sys.exit("No UI file") 
-from ui_calc import Ui_MainWindow
+# # .ui -> .py
+# import os
+# from subprocess import getstatusoutput
+# ui2py_flag = False
+# if os.path.exists("calc.ui") and getstatusoutput("pyuic5 calc.ui -o ui_calc.py")[0] == False:
+#     ui2py_flag = True
+# if ui2py_flag == False and os.path.exists("ui_calc.py") == False:
+#     sys.exit("No UI file") 
+# from ui_calc import Ui_MainWindow
 
-class UI(QMainWindow, Ui_MainWindow):
-    def __init__(self, parent=None):
-        super(UI, self).__init__(parent)
-        self.setupUi(self)
-
-# # Load UI from .ui file directly
-# class UI(QMainWindow):
+# class UI(QMainWindow, Ui_MainWindow):
 #     def __init__(self, parent=None):
 #         super(UI, self).__init__(parent)
-#         uic.loadUi("calc.ui", self)
+#         self.setupUi(self)
+
+# Load UI from .ui file directly
+class UI(QMainWindow):
+    def __init__(self, parent=None):
+        super(UI, self).__init__(parent)
+        uic.loadUi("calc.ui", self)
 
 def main():
     app = QApplication(sys.argv)
